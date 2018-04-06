ed303413379d3746ff4762881cecde75b3df4af0
iomegak12 <jd.ramkumar@gmail.com>
2018-04-06 16:32:56 +0530

    first commit

---
+++ create-sql-schema.sql
@@ -0,0 +1,16 @@
+CREATE TABLE [dbo].[mobiledata](
+[clientid] [nvarchar](50),
+[querytime] [nvarchar](50),
+[market] [nvarchar](50),
+[deviceplatform] [nvarchar](50),
+[devicemake] [nvarchar](50),
+[devicemodel] [nvarchar](50),
+[state] [nvarchar](50),
+[country] [nvarchar](50),
+[querydwelltime] [float],
+[sessionid] [bigint],
+[sessionpagevieworder] [bigint])
+GO
+
+CREATE CLUSTERED INDEX mobiledata_clustered_index on mobiledata(clientid)
+GO
